dd8cea850d71ea1fb61f7971e261163055807407
nicolashedoire <nicolashedoire@hotmail.com>
2020-06-27 22:05:29 +0200

    add cities

--- database/mongo-init.js
+++ database/mongo-init.js
@@ -15,6 +15,10 @@ db.createUser(
 
 db.city.insertMany([
     {
+        name: 'Angers',
+        country: 'France'
+      },
+    {
       name: 'Lille',
       country: 'France'
     },
@@ -23,12 +27,88 @@ db.city.insertMany([
         country: 'France'
     },
     {
+        name: 'Dijon',
+        country: 'France'
+    },
+    {
         name: 'Arras',
+        country: 'France'
+    },
+    {
+        name: 'Bordeaux',
         country: 'France'
     },
     {
         name: 'Besançon',
         country: 'France'
+    },
+    {
+        name: 'Grenoble',
+        country: 'France'
+    },
+    {
+        name: 'Lyon',
+        country: 'France'
+    },
+    {
+        name: 'Le Havre',
+        country: 'France'
+    },
+    {
+        name: 'Marseille',
+        country: 'France'
+    },
+    {
+        name: 'Montpellier',
+        country: 'France'
+    },
+    {
+        name: 'Nice',
+        country: 'France'
+    },
+    {
+        name: 'Nantes',
+        country: 'France'
+    },
+    {
+        name: 'Nîmes',
+        country: 'France'
+    },
+    {
+        name: 'Paris',
+        country: 'France'
+    },
+    {
+        name: 'Rennes',
+        country: 'France'
+    },
+    {
+        name: 'Reims',
+        country: 'France'
+    },
+    {
+        name: 'Strasbourg',
+        country: 'France'
+    },
+    {
+        name: 'Saint-Etienne',
+        country: 'France'
+    },
+    {
+        name: 'Toulon',
+        country: 'France'
+    },
+    {
+        name: 'Toulouse',
+        country: 'France'
+    },
+    {
+        name: 'Bruxelles',
+        country: 'Belgique'
+    },
+    {
+        name: 'Liège',
+        country: 'Belgique'
     },
     {
       name: 'Mons',
